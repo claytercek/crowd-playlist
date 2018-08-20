0f0ca522e31184904538ab63c72200d8c229e334
Clay Tercek <clay.tercek@gmail.com>
2018-08-20 09:46:46 -0400

    fix 100vh issue on safari ios

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from "react";
 import Connect from "./pages/connect";
 import Guest from "./pages/guest";
 import Host from "./pages/host";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 class App extends Component {
 	render() {
 		return (
 			<Router>
-				<div>
+				<Switch>
 					<Route exact path="/" component={Connect} />
 					<Route path="/host/:id/:access_token" component={Host} />
 					<Route path="/guest/:id/:name" component={Guest} />
-				</div>
+				</Switch>
 			</Router>
 		);
 	}
